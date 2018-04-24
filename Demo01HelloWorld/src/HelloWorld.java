9444ab11c23493fcb06be389159a3e5ac2c46ef1
dave <dave@armchair.mb.ca>
2018-04-24 09:01:04 +0100

    World order restored.

--- Demo01HelloWorld/src/HelloWorld.java
+++ Demo01HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	// Note that per Java style convention, the initial curly brace goes at the end of the line.
 	public static void main(String[] args) {
 		// This is the equivalent to C#'s Console.WriteLine
-		System.out.println("Jello, Whirled");
+		System.out.println("Jello Whirled!");
 	}
 
 }
